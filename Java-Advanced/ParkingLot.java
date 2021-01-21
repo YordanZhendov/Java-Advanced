8314ecfe8d3c1405a9b9e5dc6a05158475b90a94
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-01-21 17:08:57 +0200

    Add files via upload

---
+++ Java-Advanced/ParkingLot.java
@@ -0,0 +1,40 @@
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+
+        LinkedHashSet<String> cars=new LinkedHashSet<>();
+
+        String input=scanner.nextLine();
+        while (!"END".equals(input)){
+            String[] tokens=input.split(", ");
+            String command=tokens[0];
+            String carNumber=tokens[1];
+
+            if("IN".equals(command)){
+                if(!cars.contains(carNumber)){
+                    cars.add(carNumber);
+                }else {
+                    input=scanner.nextLine();
+                    continue;
+                }
+            }else{
+                if(cars.contains(carNumber)){
+                    cars.remove(carNumber);
+                }else {
+                    input=scanner.nextLine();
+                    continue;
+                }
+            }
+
+            input=scanner.nextLine();
+        }
+
+        for (String car : cars) {
+            System.out.println(car);
+        }
+    }
+}
