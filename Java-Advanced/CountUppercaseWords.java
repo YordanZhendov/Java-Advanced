4eebd70bca7e893e8ec04ee242014b7bc37b06f0
Jordan <68518634+YordanZhendov@users.noreply.github.com>
2021-01-31 00:56:44 +0200

    Add files via upload

---
+++ Java-Advanced/CountUppercaseWords.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+
+public class CountUppercaseWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] listInput = scanner.nextLine().split(" ");
+        Predicate<String> isUpper= n->Character.isUpperCase(n.charAt(0));
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < listInput.length; i++) {
+            if(isUpper.test(listInput[i])){
+                result.add(listInput[i]);
+            }
+        }
+
+        Consumer<String> print= System.out::println;
+        System.out.println(result.size());
+        result.forEach(print);
+
+    }
+}
